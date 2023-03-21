1ed2d8f079c825f72ab315248831c572690bd23d
Kiefer Levine <kiefer_8@hotmail.com>
2023-03-21 15:46:57 -0400

    Cleaned up App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,8 @@
 import { Board } from "./components";
-import {
-  BoardData,
-  boardData, 
-  getLocalStorage, 
-} from "./utils";
 
-const App = () => {
-    const localStorage:string | null = getLocalStorage('boardData') || null;
-    const userData:BoardData = localStorage !== null ? JSON.parse(localStorage) : boardData;
-
-    return (
-      <div className='d-flex justify-content-center h-100'>
-        <Board columns={userData.columns}></Board>
-      </div>
-    )
-}
+const App = () => 
+  <div className='d-flex justify-content-center h-100'>
+    <Board></Board>
+  </div>
 
 export default App;
